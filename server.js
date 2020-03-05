101979caeaf872aba856d70a4b8cd1757ba97aa9
Naillik13 <killian.galea0@orange.fr>
2020-03-05 08:46:36 +0100

    [FEAT] Add migrations to cassandra

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const client = require('./app/database/client.cassandra');
 
 // create express app
 const app = express();
@@ -11,15 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-const query = `CREATE TABLE IF NOT EXISTS sensors(name text PRIMARY KEY);`;
-client.execute(query).then(_ => {
-    console.log("Table created");
-    const api = require("./app/routes/api");
-    app.use('/api', api);
+const api = require("./app/routes/api");
+app.use('/api', api);
 
-    // listen for requests
-    app.listen(port, () => {
-        console.log("Server is listening on port 3000");
-    });
+// listen for requests
+app.listen(port, () => {
+    console.log("Server is listening on port 3000");
 });
+
 
